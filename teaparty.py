1a220f7fae8fb421fb7eb975e97da5e3b3313bba
roxcoldiron <roxcoldiron@users.noreply.github.com>
2020-02-09 17:20:36 -0500

    updated argument names in the function

--- teaparty.py
+++ teaparty.py
@@ -12,8 +12,8 @@ cups_of_tea = int(input())
 
 # function to compare to the computer's choice of how many guests we have at the tea party
 
-def cakes_and_tea(cake_count, cups_of_tea):
-  if cake_count and cups_of_tea >= num_guests:
+def cakes_and_tea(cakes, tea):
+  if cakes and tea >= num_guests:
     print(f"we have enough cakes and tea for our {num_guests} guests")
   else:
     print(f"we need more cakes and tea for our {num_guests} guests!")
